5b30b0689262f22928b78f6601a8bd586b4ab72f
Sandeep CC <72352975+Sandeepvsh369@users.noreply.github.com>
2022-08-23 11:37:40 +0530

    My code changes

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is the Default Page of the Server. If You have not specified anything or a Address, This is the page that is visible.</p></body></html>');
+        res.write('<html><body><p>I am going to be a man who dreamed of</p></body></html>');
         res.end();
 
     }
     else if (req.url == "/home") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is Home Page.</p></body></html>');
+        res.write('<html><body><p>Always</p></body></html>');
         res.end();
 
     }
