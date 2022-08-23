f50b4a9f2a9fd937221af13f1bb493aa8b2fc81f
Sandeep CC <72352975+Sandeepvsh369@users.noreply.github.com>
2022-08-23 12:16:06 +0530

    new changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>I am going to be a man who dreamed of</p></body></html>');
+        res.write('<html><body><p>Tesla is alien</p></body></html>');
         res.end();
 
     }
